be26379eefc68c728b7b7fd60dc86b5d5f6c516b
Evgenii Sh <79617771614@yandex.ru>
2025-11-27 11:58:51 +0500

    Update rms.cpp
    Error NULL

--- 01_week/tasks/rms/rms.cpp
+++ 01_week/tasks/rms/rms.cpp
@@ -5,7 +5,7 @@
 double CalculateRMS(double values[], size_t size) {
     double sum = 0.0;
     
-    if(size == 0) return 0.0;//
+    if(0 == size) return 0.0;//
     
     for(size_t i = 0; i < size; i++){
         sum += values[i]*values[i];
