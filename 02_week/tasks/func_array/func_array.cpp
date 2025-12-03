8d1e30df79f61945c993633b15346aff2aa3eb65
Evgenii Sh <79617771614@yandex.ru>
2025-12-03 09:39:46 +0500

    Update func_array.cpp

--- 02_week/tasks/func_array/func_array.cpp
+++ 02_week/tasks/func_array/func_array.cpp
@@ -1,6 +1,17 @@
 #include <stdexcept>
 
 
-double ApplyOperations(double a, double b /* other arguments */) {
-    throw std::runtime_error{"Not implemented"};
+double ApplyOperations(double a, double b, FuncTypeTest operations[], size_t size) {
+    if (operations == nullptr || size == 0) {    // Если указатель на nullptr или нулевой размер массива функций то -ошибка возвращаем 0
+        return 0.0;
+    }
+    // выход за пределы массива функций не контролируем. 
+    
+    double total_sum = 0.0;    // результат всех операций.
+    
+    for (size_t i = 0; i < size; ++i) {    // если более одной операции, проходим все.
+        total_sum += operations[i](a, b);    // учет результата операции
+    }
+    
+    return total_sum;    // возвращаем результат    
 }
