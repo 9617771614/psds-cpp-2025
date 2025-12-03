ebc17a1a8cc97f9f614ba0f29f6feab3a0c1ce8b
Evgenii Sh <79617771614@yandex.ru>
2025-12-03 09:51:59 +0500

    Update func_array.cpp

--- 02_week/tasks/func_array/func_array.cpp
+++ 02_week/tasks/func_array/func_array.cpp
@@ -3,7 +3,7 @@
 using FuncTypeTest = double (*)(double, double);
 
 double ApplyOperations(double a, double b, FuncTypeTest operations[], size_t size) {
-    if (operations == nullptr || size == 0) {    // Если указатель на nullptr или нулевой размер массива функций то -ошибка возвращаем 0
+    if (size == 0) {    //  Если нулевой размер массива функций то -ошибка возвращаем 0
         return 0.0;
     }
     // выход за пределы массива функций не контролируем. 
@@ -11,6 +11,7 @@ double ApplyOperations(double a, double b, FuncTypeTest operations[], size_t siz
     double total_sum = 0.0;    // результат всех операций.
     
     for (size_t i = 0; i < size; ++i) {    // если более одной операции, проходим все.
+        if(operations[i] == nullptr)  return 0.0;  //  Если указатель на nullptr -ошибка возвращаем 0
         total_sum += operations[i](a, b);    // учет результата операции
     }
     
